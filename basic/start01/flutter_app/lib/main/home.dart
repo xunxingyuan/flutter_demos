91b439cbd2c0347debd6c93719b75d1d70a7cf59
xunxingyuan <xunxingyuan@gmail.com>
2018-12-26 17:56:30 +0800

    add tab

--- basic/start01/flutter_app/lib/main/home.dart
+++ basic/start01/flutter_app/lib/main/home.dart
@@ -4,10 +4,69 @@ import '../common/searchBar.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: SearchBar()),
-      body: Center(
-        child: Text('首页'),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: SearchBar(),
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                  // icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: '推荐', icon: Icons.directions_car),
+  const Choice(title: 'NBA', icon: Icons.directions_bike),
+  const Choice(title: 'LOL', icon: Icons.directions_boat),
+  const Choice(title: '足球', icon: Icons.directions_bus),
+  const Choice(title: '新闻', icon: Icons.directions_railway),
+  const Choice(title: 'CBA', icon: Icons.directions_walk),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(choice.title),
+          ],
+        ),
       ),
     );
   }
